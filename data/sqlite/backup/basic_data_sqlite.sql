e0a1b439f1d52427ac073861764e3b28290adaf1
Paul Lovis Maximilian Trüstedt <truestedt@uni-potsdam.de>
2019-11-17 10:55:50 +0100

    the first order ly filestructure

--- data/sqlite/backup/basic_data_sqlite.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO `categories` (`name`, `description`) VALUES
-('pens', 'good old pens');
-
-INSERT INTO `inventory` (`position`, `product_id`) VALUES
-(1, 1);
-
-INSERT INTO `products` (`product_id`, `name`, `category_id`, `weight`, `prize`) VALUES
-(1, 'PenXL', 1, 10, 100);
