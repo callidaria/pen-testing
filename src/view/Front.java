6ed92a29ed51341afdd60e9a2149a950adbcfc9e
lucas <lucas2312@web.de>
2019-12-27 18:15:07 +0100

    Frontend

--- src/view/Front.java
+++ src/view/Front.java
@@ -19,8 +19,11 @@ import java.awt.FlowLayout;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.GridLayout;
+import java.awt.Point;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 
 public class Front extends JFrame {
@@ -28,19 +31,20 @@ public class Front extends JFrame {
 	public Front()
 	{	
 		//GUI
-		setTitle("Bestands�bersicht");
+		setTitle("Bestandsübersicht");
 		setSize(400,220);
 		setLocationRelativeTo(null);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		
 		
 		//Button
-		JButton button = new JButton ("Ende");
+		JButton button = new JButton ("New Article");
 		button.addActionListener(new ActionListener() { 
 		
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				System.exit(0);
+				NewArticle neu = new NewArticle();
+				neu.setVisible(true);
 			}
 			
 		});
@@ -58,14 +62,12 @@ public class Front extends JFrame {
                 "Anzahl"};
 		
 		Object[][] data = {
-				{"Bereich",  "Regal",
-					 "Platz", "Produktname", "Anzahl"},
 			    {new Integer(5), new Integer(5),
 			    	new Integer(5), "Roter Stift", new Integer(10000)},
 			    {new Integer(5), new Integer(5),
 				    	new Integer(5), "Blauer Stift", new Integer(500)},
 			    {new Integer(5), new Integer(5),
-					    	new Integer(5), "Gr�ner Stift", new Integer(3000)}
+					    	new Integer(5), "Grüner Stift", new Integer(3000)}
 		};
 		
 		JTable table = new JTable(data, columnNames);
@@ -73,11 +75,27 @@ public class Front extends JFrame {
 		JScrollPane scrollPane = new JScrollPane(table);
 		table.setFillsViewportHeight(true);
 		
+		table.addMouseListener(new MouseAdapter(){
+			 public void mousePressed(MouseEvent mouseEvent) {
+			        JTable table =(JTable) mouseEvent.getSource();
+			        Point point = mouseEvent.getPoint();
+			        int row = table.rowAtPoint(point);
+			        if (mouseEvent.getClickCount() == 2 && table.getSelectedRow() != -1) {
+			        	ArticleView article = new ArticleView();
+						article.setVisible(true);
+			        	//NewArticle neu = new NewArticle();
+						//neu.setVisible(true);
+			        }
+			    }	
+		});
+		
+		
+		
 		
 		//Menubar
 		JMenuBar menu = new JMenuBar();
 		JMenu datei = new JMenu("Datei");
-		JMenu submenu = new JMenu("Submen�");
+		JMenu submenu = new JMenu("Submenü");
 		JMenuItem eins = new JMenuItem("Kategorie");
 		JMenuItem zwei = new JMenuItem("Punkt 2");
 		eins.addActionListener(new ActionListener() { 
@@ -94,7 +112,7 @@ public class Front extends JFrame {
 			
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				JOptionPane.showMessageDialog(getContentPane(),"Hier k�nnte ihre Nachricht stehen","�berschrift", JOptionPane.INFORMATION_MESSAGE);
+				JOptionPane.showMessageDialog(getContentPane(),"Hier könnte ihre Nachricht stehen","�berschrift", JOptionPane.INFORMATION_MESSAGE);
 			}
 			
 		});
