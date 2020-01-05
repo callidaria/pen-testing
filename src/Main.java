565198359cb2fdc570a90fc06d7dd5553f819efd
Freddy@Desktop <youfreddi@gmail.com>
2020-01-05 21:35:21 +0100

    nothing to see here either

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,10 @@
 public class Main {
 	public static void main(String[] args) {
-		
-		
 		new DatabaseExamples();
 		System.out.println("\n---END OF EXAMPLE---\n");
 		view.Front app = new view.Front();
 		app.setVisible(true);
 	}
-	
 	
 	
 }
