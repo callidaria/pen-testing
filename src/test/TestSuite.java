527f0e15da6f4f05d00910f7d9f150488f76728d
Freddy@Desktop <youfreddi@gmail.com>
2020-02-15 15:37:51 +0100

    TestSuite added

--- src/test/TestSuite.java
+++ src/test/TestSuite.java
@@ -1,5 +1,16 @@
 package test;
 
-public class TestSuite {
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
 
+@RunWith(Suite.class)
+
+@Suite.SuiteClasses({
+MyClassTest.class,
+})
+
+public class TestSuite {
+	public TestSuite() {
+		
+	}
 }
